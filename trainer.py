e43c28dcaa48839bedc2b3d5df6f91efe284dbc6
kdhsimplepro <kdhsimplepro@gmail.com>
2023-01-27 14:50:52 +0900

    modify

--- trainer.py
+++ trainer.py
@@ -53,31 +53,40 @@ class Trainer:
 
     def __init__(
         self,
-        gen,
-        disc,
-        lr=1e-3
+        gen_state_dict,
+        disc_state_dict,
+        z_dim=512,
+        w_dim=512,
+        lr=1e-3,
+        betas=(0.5, 0.99),
+        alpha=1e-7,
+        step=0,
+        could_gp=True
     ):
 
-        self.gen = gen.to(DEVICE)
-        self.disc = disc.to(DEVICE)
+        self.gen = Generator(z_dim, w_dim, const_channels=512).to(DEVICE)
+        self.disc = Discriminator().to(DEVICE)
 
+        if gen_state_dict:
+            self.gen.load_state_dict(gen_state_dict)
+
+        if disc_state_dict:
+            self.disc.load_state_dict(disc_state_dict)
+        
         self.opt_gen = Adam([
             {"params": [param for name, param in self.gen.named_parameters() if 'mapping_network' not in name]},
             {"params": self.gen.mapping_network.parameters(), 'lr': lr*(1e-2)}
-        ], lr=lr, betas=(0.0, 0.99))
+        ], lr=lr, betas=betas)
     
-        self.opt_disc = Adam(self.disc.parameters(), lr=lr, betas=(0.0, 0.99))
-
+        self.opt_disc = Adam(self.disc.parameters(), lr=lr, betas=betas)
 
         self.test_z = torch.randn((16, 512)).to(DEVICE)
 
-        self.reset_alpha()
+        self.alpha = alpha
 
-        self.step = 0
+        self.step = step
 
-    
-    def reset_alpha(self):
-        self.alpha = 1e-7
+        self.could_gp = could_gp
 
     
     @torch.no_grad()
@@ -109,16 +118,26 @@ class Trainer:
 
             disc_real = self.disc(real, self.alpha, self.step)
             disc_fake = self.disc(fake.detach(), self.alpha, self.step)
-            gp = get_gradient_penalty(self.disc, real, fake, self.alpha, self.step, DEVICE)
+
+            if self.could_gp:
+                gp = get_gradient_penalty(self.disc, real, fake, self.alpha, self.step, DEVICE)
+
             disc_loss = (
                 -(disc_real.mean() - disc_fake.mean())
-                + args["LAMBDA_GP"] * gp
                 + (0.001) * torch.mean(disc_real ** 2)
             )
+
+            if self.could_gp:
+                disc_loss += args["LAMBDA_GP"] * gp
 
             self.opt_disc.zero_grad()
             disc_loss.backward()
             self.opt_disc.step()
+
+            # 이 부분을 추가하는 것이 좋을 듯.
+            # gen이 만든 fake에 대해서 가중치 update가 이루어진 disc에게, 다시 fake에 대한 예측을 받아서 gen의 loss를 만들어내는 것은 discriminator에게 아주 유리한 조건이기 때문이다.
+            z = torch.randn(real.size(0), args["Z_DIM"]).to(DEVICE)
+            fake = self.gen(z, self.alpha, self.step)
 
             gen_fake = self.disc(fake, self.alpha, self.step)
             gen_loss = -gen_fake.mean()
@@ -131,32 +150,29 @@ class Trainer:
             self.alpha = min(self.alpha, 1)
 
             loop.set_postfix(
-                gp=gp.item(),
+                gp=gp.item() if self.could_gp else None,
                 gen_loss=gen_loss.item(),
                 disc_loss=disc_loss.item()
             )
 
-            wandb.log({"gp": gp.item(), "gen_loss": gen_loss.item(), "disc_loss": disc_loss.item()})
+            wandb.log({"gen_loss": gen_loss.item(), "disc_loss": disc_loss.item()})
+            if self.could_gp:
+                wandb.log({"gp": gp.item()})
 
 
-    def run(self, step, epochs, loader):
-        print(f"\n\nImage Size: {args['IMAGE_SIZES'][step]}x{args['IMAGE_SIZES'][step]}\n")
-        self.step = step
-        wandb.log({"step": step})
+    def run(self, epochs, loader):
+        print(f"\n\nImage Size: {args['IMAGE_SIZES'][self.step]}x{args['IMAGE_SIZES'][self.step]}\n")
 
         for epoch in range(epochs):
             wandb.log({"cur_epoch": epoch+1})
             print(f"EPOCH: {epoch+1}/{epochs}")
             self.train_fn(epochs, loader)
             test_image = self.test_fn()
-            test_image.save(f"test_images/{step}/{epoch}.jpg")
-            wandb.log({f"test_image{step}": wandb.Image(test_image)})
+            test_image.save(f"test_images/{self.step}/{epoch}.jpg")
+            wandb.log({f"test_image{self.step}": wandb.Image(test_image)})
 
             torch.save(self.gen.state_dict(), "./gen_state_dict.pt")
             torch.save(self.disc.state_dict(), "./disc_state_dict.pt")
-        
-        
-        self.reset_alpha()
 
 
 if __name__ == '__main__':
@@ -168,15 +184,34 @@ if __name__ == '__main__':
 
     wandb.config.update(args)
 
-    gen = Generator(args["Z_DIM"], args["W_DIM"], const_channels=512)
-    disc = Discriminator()
-
-    trainer = Trainer(gen, disc)
+    gen_state_dict = None
+    disc_state_dict = None
 
     for step in range(8):
+
+        if step != 0:
+            gen_state_dict = torch.load(f"./gen_state_dict_step{step-1}.pt")
+            disc_state_dict = torch.load(f"./disc_state_dict_step{step-1}.pt")
+
+        trainer = Trainer(
+            gen_state_dict=gen_state_dict,
+            disc_state_dict=disc_state_dict,
+            z_dim=512,
+            w_dim=512,
+            lr=0.0002,
+            betas=(0.5, 0.99),
+            alpha=1e-7,
+            step=step,
+            could_gp=True
+        )
+
         loader, _ = get_loader(
             args["IMAGE_SIZES"][step],
             dataset_root="/home/kdhsimplepro/kdhsimplepro/AI/ffhq/",
             batch_size=args["BATCH_SIZES"][step]
         )
+
         trainer.run(step=step, epochs=args["EPOCHS"][step], loader=loader)
+
+        torch.save(trainer.gen.state_dict(), f"./gen_state_dict_step{step}.pt")
+        torch.save(trainer.disc.state_dict(), f"./disc_state_dict_step{step}.pt")
